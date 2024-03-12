844bdc55d547820134555ecd551d4d3827fa042c
wlh <976724250@qq.com>
2024-03-12 13:28:28 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 
 Route::resource('users', 'UsersController', ['only' => ['show','edit','update']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
